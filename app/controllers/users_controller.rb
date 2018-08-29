d04a012339c36aae818ff00b255e0d70a705c79d
Aellon Krider <aellonkrider@gmail.com>
2018-08-29 16:42:47 -0400

    Update users new view and users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to user_path(@user.id)
+		end
+	end
+
+	def show
+		
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password_digest)
+	end
+
+
+end
