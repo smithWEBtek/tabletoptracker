65b041f792bf951e72928418519a6897f04f063f
Aellon Krider <aellonkrider@gmail.com>
2018-09-05 12:03:59 -0400

    Add routes for sessions and add a sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-	resources :users
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+	resources :users, path_names: { new: 'signup'}
 	resources :games
 	resources :matches
+	root :to => 'welcome#index'
+    get '/signin' => 'sessions#new'
+    post '/signin' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
 end
