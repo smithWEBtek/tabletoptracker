15aa5f36ac14a1e9c3f014f5d41f7f082bb6fa07
Aellon Krider <aellonkrider@gmail.com>
2018-09-05 13:30:01 -0400

    update routes for signup path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 	get '/signin' => 'sessions#new'
     post '/signin' => 'sessions#create'
     get '/logout' => 'sessions#destroy'
-	resources :users, path_names: { new: 'signup'}
+    get '/signup' => 'users#new'
+	resources :users
 	resources :games
 	resources :matches
 	root :to => 'welcome#index'
