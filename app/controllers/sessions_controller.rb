bf1274e9d415d9a84f88abb491f76c0e6dd06902
Aellon Krider <aellonkrider@gmail.com>
2018-09-05 12:34:15 -0400

    update sessions new view, add bcrypt to gemfile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	@user = User.find_by(email: params[:user][:email])
+
+  	if @user && @user.authenticate(params[:user][:password])
+  		session[:user_id] = @user.user_id
+  		redirect_to user_path(@user)
+  	else
+  		redirect_to root_path
+  	end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+	get '/signin' => 'sessions#new'
+    post '/signin' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
 	resources :users, path_names: { new: 'signup'}
 	resources :games
 	resources :matches
 	root :to => 'welcome#index'
-    get '/signin' => 'sessions#new'
-    post '/signin' => 'sessions#create'
-    get '/logout' => 'sessions#destroy'
+    
 end
