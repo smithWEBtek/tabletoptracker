db4b6c05cfe2bfc1b2414a14d9e004c4ecf6682d
Aellon Krider <aellonkrider@gmail.com>
2018-09-05 12:14:38 -0400

    Create welcome controller for root path, update view for welcome#index, and move signup form to sessions#new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
   def new
+  	@user = User.new
   end
 
   def create
   end
 
   def destroy
+  	session.clear
+  	redirect to root_path
   end
 end
