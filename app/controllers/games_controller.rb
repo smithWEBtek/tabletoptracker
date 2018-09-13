abcb28bf23e46bf203f5cf9c3f2e08e546b288d7
Aellon Krider <aellonkrider@gmail.com>
2018-09-13 11:56:01 -0400

    Update actions in Games Controller and add a form to the new games views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,21 @@
 class GamesController < ApplicationController
 	def new
-		
+		@game = Game.new
 	end
 
 	def create
-		
+		@game = Game.create(game_params)
+		redirect_to game_path(@game)
 	end
 
 	def show
-		
+		@game = Game.find(params[:id])
+	end
+
+	private
+
+	def game_params
+		params.require(:game).permit(:name)
 	end
 
 end
