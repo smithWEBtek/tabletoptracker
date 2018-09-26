c4647e9aa3913390d59fb45f0c8634488842799c
Aellon Krider <aellonkrider@gmail.com>
2018-09-26 00:31:28 -0400

    fb omniauth debugging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,28 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
-  	# @user = User.find_or_create_by(uid: auth['uid']) do |u|
-   #    u.name = auth['info']['name']
-   #    u.email = auth['info']['email']
-   #    u.image = auth['info']['image']
-   #  end
+    if auth
+  	 @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+      u.password = SecureRandom.hex
+      
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    end
 
-  	if @user && @user.authenticate(params[:user][:password])
+  	else
+      @user = User.find_by(email: params[:user][:email])
+
+      if @user && @user.authenticate(params[:user][:password])
   		session[:user_id] = @user.id
   		redirect_to user_path(@user)
-  	else
+  	  else
       flash[:alert] = "Invalid email/password combination"
       redirect_to root_path
-  	 end
+  	  end
+    end
   end
 
   def destroy
