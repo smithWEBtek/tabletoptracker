966b194fe9db0ed0dfccf088a5ea0c657dd3e3cd
Aellon Krider <aellonkrider@gmail.com>
2018-10-02 11:36:35 -0400

    Update name of User helper method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,5 +1,5 @@
 module UserHelper
-	def user_path(current_user)
+	def current_user_page(current_user)
 		if request.path.include?("#{current_user.id}")
 			link_to('Add a match here', new_game_path)
 		end
