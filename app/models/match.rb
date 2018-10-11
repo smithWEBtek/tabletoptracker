ad76f983d792eb705bf9c70ca2cfe36f3f50035c
Aellon Krider <aellonkrider@gmail.com>
2018-10-11 15:32:09 -0400

    Update match model to be able to edit scythe data when updating record

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,7 @@
 class Match < ApplicationRecord
 	belongs_to :game
 	belongs_to :user
-	has_one :scythe, inverse_of: :match
+	has_one :scythe, inverse_of: :match, :dependent => :destroy
 	accepts_nested_attributes_for :scythe
 end
 
