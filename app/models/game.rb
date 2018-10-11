e6ba4819794a51522f267a397362797b0e8675cc
Aellon Krider <aellonkrider@gmail.com>
2018-10-11 12:01:19 -0400

    Update game model favorite scope method

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,14 @@ class Game < ApplicationRecord
 	has_many :users, through: :matches
 	validates :name, uniqueness: true
 	scope :favorite, -> {where games = self.all.collect { |game|
-	 	game.matches.count
-	 	game.name
+	 	if
+	 		game.matches.count > 1
+	 		game.matches.count
+	 		game.name
+	 	else
+	 		"going to be the one that you play the most"
+	 	end
+	 	
 	 }
 
 	 games.sort_by(&:to_i).last
